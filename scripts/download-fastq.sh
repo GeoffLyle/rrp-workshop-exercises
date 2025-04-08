69e916e5fd2eb7fb87a6b975a54749f6757dbef6
GeoffLyle <geofflyle@gmail.com>
2025-04-08 11:59:44 -0700

    Add script to download FASTQ data
    Add download-fastq.sh

---
+++ scripts/download-fastq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Set options
+set -euo pipefail
+
+# Run from the directory where this file lives
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Set variables
+DATA_PATH="../data/raw/fastq/SRP255885"
+FQ_FILE1="SRR11518889_1.fastq.gz"
+FQ_FILE2="SRR11518889_2.fastq.gz"
+FQ_FILE_PATH1="ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889/${FQ_FILE1}"
+FQ_FILE_PATH2="ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889/${FQ_FILE2}"
+
+# Create directory for raw fastq data
+mkdir -p $DATA_PATH
+
+# Download the fastq data
+if [ ! -e "$DATA_PATH/$FQ_FILE1" ]; then
+
+	echo "Downloading ${FQ_FILE1}"
+	
+	# Download data from ebi
+	curl --output $DATA_PATH/$FQ_FILE1 --url $FQ_FILE_PATH1
+fi
+
+if [ ! -e "$DATA_PATH/$FQ_FILE2" ]; then
+
+	echo "Downloading ${FQ_FILE1}"
+	
+	# Download data from ebi
+	curl --output $DATA_PATH/$FQ_FILE2 --url $FQ_FILE_PATH2
+fi
+
+# Calculate number of lines in .fastq.gz files
+OUTPUT_LINES1=`gunzip -c $DATA_PATH/$FQ_FILE1 | wc -l` 
+echo "The number of lines in ${FQ_FILE1} is: ${OUTPUT_LINES1}"
+
+OUTPUT_LINES2=`gunzip -c $DATA_PATH/$FQ_FILE2 | wc -l` 
+echo "The number of lines in ${FQ_FILE2} is: ${OUTPUT_LINES2}"
+
